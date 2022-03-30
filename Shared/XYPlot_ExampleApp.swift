6b264f2eeb10ec4c4c859274f7701eb56b550e83
Joseph Levy <josephlevy222@gmail.com>
2022-03-30 08:34:45 -0700

    Initial Commit

---
+++ Shared/XYPlot_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XYPlot_ExampleApp.swift
+//  Shared
+//
+//  Created by Joseph Levy on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct XYPlot_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
